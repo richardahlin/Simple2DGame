5b456db0af48f746c39c471fa627192470ec8dfe
AxelArkheden <axel.arkheden@gmail.com>
2016-12-10 23:57:54 +0100

    Modified main func with print and updated gitignore.

--- src/simple2dgame/Simple2DGame.java
+++ src/simple2dgame/Simple2DGame.java
@@ -15,6 +15,7 @@ public class Simple2DGame {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("test");
         // TODO code application logic here
     }
     
